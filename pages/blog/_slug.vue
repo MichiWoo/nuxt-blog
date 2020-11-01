c7e4e3bd2adb377be0b2b16cbe8687fead0f9529
MichiWoo <michiwoo.web@gmail.com>
2020-11-01 17:03:51 -0600

    🐛 FIX: Correcion tirulo de slug de blog

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div(class='flex justify-center item-align-center')
     article(class='flex flex-col pt-8 pb-8 w-3/4 border-b-2 border-light-accent dark:border-dark-accent')
-      h1(class='text-2xl pl-4 py-4 inline rounded-tl-lg rounded-br-lg bg-gradient-to-r from-light-danger via-light-accent to-light dark:from-dark-accent dark:via-dark dark:to-dark font-bold leading-tight text-light-primary dark:text-dark-secondary')
+      h1(class='text-2xl pl-4 py-4 inline rounded-tl-lg rounded-br-lg bg-gradient-to-r from-light-danger via-light-accent to-light dark:from-dark dark:via-dark dark:to-dark font-bold leading-tight text-light-primary dark:text-dark-secondary')
         | {{ blo.title }}
       b(class="my-4 text-light-danger dark:text-dark-danger font-bold") {{ formatDate(blo.updatedAt) }} - 1 min read
       div.separator
